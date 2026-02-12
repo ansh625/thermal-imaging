59a3e1b538327b45d5b85355e0e4728732a637e3
ansh <anshpreetkaurts90@gmail.com>
2026-02-12 10:12:41 +0530

    Ignore editor history

--- .history/frontend/src/App_20260211150832.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind v4 is working 🚀
-      </h1>
-    </div>
-  )
-}
